2a7457e596ab3d0d4adb612182cff2ffc2d9ee2b
Tanvi Naik <67183737+TanvNaik@users.noreply.github.com>
2021-02-17 20:49:00 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+"use strict";
+
+console.log("Hello World")
+
+let date = document.querySelector(".date").textContent;
+
+let addTask = document.querySelector(".add");
+
+
+
+
+
+const controller = function(){
+
+    function eventlistners(){
+        addTask.addEventListener(click,addNewTask)
+    }
+}
